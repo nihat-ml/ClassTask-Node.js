a3b6f6146d2e4c041ba3d35fbcce39e873243a12
ClassTime <classtime@code.local>
2025-01-09 19:04:08 +0400

    node.js

--- FrontEnd/src/pages/Home/index.jsx
+++ FrontEnd/src/pages/Home/index.jsx
@@ -1,44 +1,60 @@
 import React, { useEffect, useState } from 'react'
+import {NavLink, useNavigate} from "react-router-dom"
+import { Table } from 'react-bootstrap';
+import axios from 'axios';
  
 function Home() {
     let [cars, setCars] = useState([]);
+   
     
 
+    function getData(){
+      axios.get("http://localhost:3001/cars")
+      .then((res)=>{
+        setCars(res.data)
+      })
+    }
     useEffect(()=>{
-        axois.get("http://localhost:3001/cars")
-        .then(res=>{
-            setCars(res.data)
-        })
+       getData()
+       console.log(cars[0])
     },[])
+    async  function handleDelete(_id){
+      await axios.delete(`http://localhost:3001/cars/${_id}`)
+      getData()
+   }
+
+
+
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
           <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Brand</th>
+          <th>Model</th>
+          <th>Year</th>
+          <th>Price</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
+        {
+                    
+          cars.map((car, index) => (
+          <tr key={car._id}>
+          <td>{index + 1}</td>  
+          <td>{car.name}</td> 
+          <td>{car.model}</td>  
+          <td>{car.year}</td>  
+          <td>{car.price}</td>
+          <td><button onClick={()=>handleDelete(car._id)}>Delete</button></td>
+          <td>
+            <NavLink to={`/detail/${car._id}`}><button>Detail</button></NavLink>
+          </td>
+          </tr>
+
+          ))
+                }
+            </tbody>
     </Table>
   )
 }
--- FrontEnd/src/routes/routes.jsx
+++ FrontEnd/src/routes/routes.jsx
@@ -1,15 +1,18 @@
+import { Children } from "react";
 import Detail from "../pages/Detail";
 import Home from "../pages/Home";
 
 const ROUTES = [
     {
         path:"/",
-        element:<Home/>
-
-    },{
-        path:"/detail",
-        element:<Detail/>
-    }
+        element:<Home/>,
+        children:[
+            {
+                path:"detail/:id",
+                element:<Detail/>
+            }
+        ]
+    },
 ]
 
 export default ROUTES
